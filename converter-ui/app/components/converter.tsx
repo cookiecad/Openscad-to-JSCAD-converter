b7aca47125035280ae0e3217c80fc3b65e43753a
Nathan Apter <napter@gmail.com>
2024-08-21 08:42:05 -0400

    added next-ui wip

--- converter-ui/app/components/converter.tsx
+++ converter-ui/app/components/converter.tsx
@@ -2,6 +2,7 @@
 // Converter.tsx
 import { useState } from 'react';
 import { parseOpenSCAD } from './parse-util';
+import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
 
 export default function Converter() {
   const [code, setCode] = useState<string>('');
@@ -27,6 +28,35 @@ export default function Converter() {
       <button className='px-4 py-2 text-white bg-blue-500 rounded' onClick={handleConvert}>
         Convert
       </button>
+      
+
+    <div className="flex w-full flex-col">
+      <Tabs aria-label="Options">
+        <Tab key="photos" title="Photos">
+          <Card>
+            <CardBody>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </CardBody>
+          </Card>  
+        </Tab>
+        <Tab key="music" title="Music">
+          <Card>
+            <CardBody>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </CardBody>
+          </Card>  
+        </Tab>
+        <Tab key="videos" title="Videos">
+          <Card>
+            <CardBody>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </CardBody>
+          </Card>  
+        </Tab>
+      </Tabs>
+    </div>  
+  );
+
     </div>
   );
 }
--- converter-ui/tailwind.config.ts
+++ converter-ui/tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
 
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +18,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui(
+    
+  )],
 };
 export default config;
