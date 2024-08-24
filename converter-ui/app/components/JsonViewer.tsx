338a424a665d16eb8d673d5bc56bd3cdac5b8fe1
Nathan Apter <napter@gmail.com>
2024-08-24 17:51:13 -0400

    Fix error when clicking on empty trees tab

--- converter-ui/app/components/JsonViewer.tsx
+++ converter-ui/app/components/JsonViewer.tsx
@@ -2,5 +2,5 @@ import JsonView from '@uiw/react-json-view';
 import React from 'react';
 
 export default function JsonViewer({ data }) {
-  return <JsonView value={data} displayDataTypes={false}/>;
+  return data && <JsonView value={data} displayDataTypes={false}/>;
 }
